e3fbe262c3e0748b4ca311a8b91a14d33034c617
Oscar <oscarpaulse@gmail.com>
2024-06-27 03:55:31 +0200

    Latest Updates to the Code

--- MeanRisk_Optimization.py
+++ MeanRisk_Optimization.py
@@ -4,24 +4,32 @@ Created on Wed Jun 26 04:32:52 2024
 
 @author: Oscar Paulse
 """
-import numpy as np
 import pandas as pd
 import yfinance as yt
-from plotly.io import show
+from plotly.io import show, write_image
+import os
+import kaleido
 from sklearn.model_selection import train_test_split
 
 from skfolio import Population, RiskMeasure
 from skfolio.optimization import EqualWeighted, MeanRisk, ObjectiveFunction
 from skfolio.preprocessing import prices_to_returns
+from sklearn.pipeline import Pipeline
+from skfolio.pre_selection import DropCorrelated
+from sklearn import set_config
+from skfolio.model_selection import (
+    CombinatorialPurgedCV,
+    cross_val_predict,
+    optimal_folds_number
+    )
 
 tickers = ['ARI.JO', 'APN.JO','EXX.JO','HAR.JO','MPT.JO','MTN.JO','OCE.JO','RBX.JO','THA.JO', 'WBO.JO']
 
-start_date = '2014-01-01'
+start_date = '2022-01-01'
 end_date = '2024-06-25'
 df = yt.download(tickers,start = start_date, end = end_date)['Adj Close']
-#returns = df.pct_change().dropna()
 returns = prices_to_returns(df)
-X_train, X_test = train_test_split(returns, test_size=0.1, shuffle=False)
+X_train, X_test = train_test_split(returns, test_size=0.3, shuffle=False)
 
 # Minimum CVaR model fitted to training data
 
@@ -39,23 +47,49 @@ weights = weights.sort_values('weights', ascending= False)
 
 benchmark = EqualWeighted(portfolio_params=dict(name="Equal Weighted"))
 benchmark.fit(X_train)
-benchmark.weights_
+
+# Removing Higly Correlated Stocks
+set_config(transform_output = "pandas")
+model2 = Pipeline([("pre_selection", DropCorrelated(threshold=0.5)),
+                  ("optimization", MeanRisk(risk_measure=RiskMeasure.CVAR, 
+                                            objective_function = ObjectiveFunction.MINIMIZE_RISK,
+                                            portfolio_params = dict(name="Min CVaR")))])
+model2.fit(X_train)
+features_selected = model2['pre_selection'].get_support(indices=False)*1
+def mask_extract(labels, mask):
+    return [s for s,m in zip(labels,mask) if m==1]
+
+weights2 = pd.DataFrame()
+weights2.index = mask_extract(tickers,features_selected)
+weights2['weights'] = model2["optimization"].weights_.round(2)*5000
+weights2 = weights2.sort_values('weights', ascending= False)
 
 
 pred_model = model.predict(X_test)
+pred_model2 = model2.predict(X_test)
 pred_bench = benchmark.predict(X_test)
 
 print(pred_model.cvar)
+print(pred_model2.cvar)
 print(pred_bench.cvar)
 
-population = Population([pred_model, pred_bench])
-population.plot_composition()
-
+population = Population([pred_model,pred_model2, pred_bench])
+summary = population.summary()
+#population.plot_composition()
 
 fig = population.plot_cumulative_returns()
-show(fig)
 
-summary = population.summary()
+"""
+if not os.path.exists("images"):
+    os.mkdir("images")
+
+import plotly.io as pio
+pio.orca.config.use_xvfb = True
+"""
+
+show(fig)#.write_image("images/fig1.png")
+
+
 
 
 
